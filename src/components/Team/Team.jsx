30d9939ad6c0973cfc34249fb61766f364d7db51
Rakishev Sanzhar <90037310+RAAK1SH@users.noreply.github.com>
2024-06-10 12:04:53 +0600

    Add files via upload

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,31 +5,31 @@ const teamData = [
   {
     id: 1,
     name: "Naz Zubia",
-    text: "GIST EECS student, honing their skills in technology and tackling challenges in the field. ",
+    text: "GIST EECS student. ",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 1,
     name: "Woletemaryam Liyew Abitew",
-    text: "GIST EECS student, honing their skills in technology and tackling challenges in the field. ",
+    text: "GIST EECS student. ",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 1,
     name: "Rakishev Sanzhar",
-    text: "GIST EECS student, honing their skills in technology and tackling challenges in the field. ",
+    text: "GIST EECS student. ",
     img: "https://picsum.photos/103/101",
   },
   {
     id: 1,
     name: "Chriv Sokuntepy",
-    text: "GIST EECS student, honing their skills in technology and tackling challenges in the field. ",
+    text: "GIST EECS student. ",
     img: "https://picsum.photos/103/104",
   },
   {
     id: 1,
     name: "Malika",
-    text: "GIST EECS student, honing their skills in technology and tackling challenges in the field. ",
+    text: "GIST EECS student. ",
     img: "https://picsum.photos/103/105",
   },
 ];
@@ -78,17 +78,10 @@ const Team = () => {
       <div data-aos="fade-up" data-aos-duration="300" className="py-10">
         <div className="container">
           {/* Header section */}
-          <div className="text-center mb-20 max-w-[400px] mx-auto">
-            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
-              {/* Teaml */}
-            </p>
-            <h1 className="text-3xl font-bold">Our team</h1>
-            <p className="text-xs text-gray-400">
-              {" "}
-              We are enthusiastic students from Gwangju Institute of Science and Technology (GIST). 
-              This project is the culmination of our hard work in the Data Engineering course,
-              where we took a deep dive into machine learning. 
-            </p>
+          <div className="text-left">
+            <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
+                Our Team
+            </h1>
           </div>
           {/* team section */}
           <div
@@ -96,7 +89,7 @@ const Team = () => {
             data-aos-duration="300"
             className="grid grid-cols-1 max-w-[800px] mx-auto gap-6"
           >
-            <Slider {...settings}>
+            <Slider {...settings} className="mb-6">
               {teamData.map(({ id, name, text, img }) => {
                 return (
                   <div key={id} className="my-6">
