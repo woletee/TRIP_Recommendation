d7d97a66ea138ed3db8c9bc148b4fc8098f950bf
Liyew <118590747+woletee@users.noreply.github.com>
2024-06-10 18:55:19 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,12 @@ import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 import PlacesRoute from "./pages/PlacesRoute";
 import About from "./pages/About";
-import Restaurants from "./pages/Restaurants"
-import RestaurantDetail from "./pages/RestaurantDetail";
-import RecommendedRestaurants from './pages/RecommendedRestaurants';
-import Restaurant from './components/Restaurant/RestaurantRecommindation';
-import ShoppingMalls from "./pages/ShoppingMalls"
-import ShoppingMallDetails from "./pages/ShoppingMallDetails"
+import Restaurants from "./pages/Restaurants";
 import Hotels from "./pages/Hotel";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import TopHotels from "./components/Hotels/TopHotels";
 const App = () => {
+  
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -33,13 +28,8 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
 
-            <Route path="/recommend" element={<RecommendedRestaurants />} />
-            <Route path="/restaurant" element={<Restaurant />} />
+            <Route path="/restaurants" element={<Restaurants />} />
 
-            <Route path="restaurants" element={<Restaurants />} />
-            <Route path="restaurants/:id" element={<RestaurantDetail />} />
-            <Route path="shopping" element={<ShoppingMalls />} />
-            <Route path="shopping/:id" element={<ShoppingMallDetails />} />
             <Route path="hotels" element={<Hotels />} />
             <Route path="best-places" element={<PlacesRoute />} />
             <Route path="about" element={<About />} />
