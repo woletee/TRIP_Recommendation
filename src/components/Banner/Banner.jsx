98292cca844ac3282f1cf2dd6aad31ec3096fa1e
Rakishev Sanzhar <90037310+RAAK1SH@users.noreply.github.com>
2024-06-10 12:17:05 +0600

    Add files via upload

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ import { IoFastFoodSharp } from "react-icons/io5";
 const Banner = () => {
   return (
     <>
-      <div className="min-h-[550px] bg-gray-100">
+      <div className="min-h-[550px]">
         <div className="min-h-[550px] flex justify-center items-center backdrop-blur-xl py-12 sm:py-0 ">
           <div className="container">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
