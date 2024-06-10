406b6c7fae90d34b24bce4da9a20562e961449fe
Liyew <118590747+woletee@users.noreply.github.com>
2024-06-10 19:01:16 +0900

    Update RestaurantCard.jsx

--- src/components/Restaurant/RestaurantCard.jsx
+++ src/components/Restaurant/RestaurantCard.jsx
@@ -1,23 +1,14 @@
-import React from "react";
+import React from "react"
+import TopRestaurants from "../components/Restaurant/TopResturant";
+import Restaurants from "../components/Restaurant/Restaurants";
+function Restaurant() {
+  return (
+    <div>
+      <section data-aos="fade-up" className="container">
+        <Restaurants />
+      </section>
+    </div>
+  );
+}
 
-const RestaurantCard = ({ img, name, location, ranking, handleRestaurantClick }) => {
-    return (
-      <div
-        className="min-w-[250px] max-w-[250px] bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-2 cursor-pointer"
-        onClick={handleRestaurantClick}
-      >
-        <img
-          src={img}
-          alt={name}
-          className="w-full h-40 object-cover"
-        />
-        <div className="p-4">
-          <h2 className="text-xl font-bold text-gray-800 mb-2">{name}</h2>
-          <p className="text-sm text-gray-600 mb-1">Location: {location}</p>
-          <p className="text-sm text-gray-600">Ranking: {ranking}</p>
-        </div>
-      </div>
-    );
-  };
-
-export default RestaurantCard;
+export default Restaurant;
