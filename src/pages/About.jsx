31749d38e13cd1989a74e8d46afe2fcea45bdf09
Rakishev Sanzhar <90037310+RAAK1SH@users.noreply.github.com>
2024-06-10 12:12:08 +0600

    Add files via upload

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,46 +5,27 @@ import Team from "../components/Team/Team";
 const About = () => {
   return (
     <>
-      <div className="container pt-14">
-        <div className="py-10">
-          <h1 className=" my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-bold">
-          About us
-          </h1>
-            <div class="about-us">
-              <h2>Travellogo: Your Journey Starts Here</h2>
-              <p>
-                At Travellogo, we believe travel is more than just a vacation; it's an opportunity to transform yourself and discover the world's wonders. We're a passionate team of travel specialists dedicated to crafting unforgettable experiences for every kind of traveler.
-              </p>
-              <style jsx>{`
-                .about-us {
-                  display: flex;
-                  flex-direction: column;
-                  align-items: center;
-                  max-width: 800px;
-                  margin: 0 auto;
-                  padding: 2rem;
-                  font-size: 18px;
-                }
-                h2 {
-                  margin-top: 0.5rem;
-                  text-align: left; 
-                  font-size: 1.5rem; 
-                  font-weight: bold; 
-                  color: #333;
-                  text-decoration: none;
-                  transition: text-decoration 0.3s ease-in-out;
-                  &:hover {
-                    text-decoration: underline;
-                  }
-                strong {
-                  font-weight: bold;
-                }
-            `}</style>
-            </div>
+      <section data-aos="fade-up" className="mt-12">
+        <div className="container my-4">
+          <div className="dark:bg-gray-900 dark:text-white bg-gray-50 pt-14">
+            <section data-aos="fade-up" className="container py-10">
+              <div className="about-us mx-auto p-6 bg-white rounded-lg shadow-md">
+              <h1 className="inline-block border-l-8 border-primary/50 py-2 pl-2 mb-4 text-xl font-bold sm:text-3xl">
+                About Us
+              </h1>
+                <h2 className="text-2xl font-bold text-gray-800 mb-4">
+                  Your Journey Starts Here
+                </h2>
+                <p className="text-lg text-gray-600">
+                  We believe travel is more than just a vacation; it's an opportunity to transform yourself and discover the world's wonders. We're a passionate team of travel specialists dedicated to crafting unforgettable experiences for every kind of traveler.
+                </p>
+              </div>
+              <Team />
+              <Location />
+            </section>
+          </div>
         </div>
-        <Team />
-      </div>
-      <Location />
+      </section>
     </>
   );
 };
