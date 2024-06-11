45a7e9996fe47c955e7ce5cc2cfd7a767c843f7d
Rakishev Sanzhar <90037310+RAAK1SH@users.noreply.github.com>
2024-06-11 09:53:06 +0600

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,6 +28,24 @@ const Home = () => {
         <h1>Hotel Booking App</h1>
       </header>
       <main>
+      <div className="relative h-[700px]">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+          >
+            <source src={NatureVid} type="video/mp4" />
+          </video>
+          <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
+            <h1 className="text-6xl font-extrabold drop-shadow-md mb-2 ">Let's</h1>
+            <h1 className="text-8xl font-extrabold drop-shadow-lg mb-2">Explore</h1>
+            <h1 className="text-5xl font-bold drop-shadow-md">Gwangju</h1>
+          </div>
+        </div>
+        <h1 className="my-8 border-l-8 border-primary/50 py-3 pl-2 text-3xl font-bold mt-20 ml-5">
+            Hotels
+        </h1>
         <TopHotels />
          <TopRestaurants/>
          <TopPlaces/>
